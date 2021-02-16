d78f45711d110ce0eb3b5891a2d679d5bc53fecd
GermanCamiloHolguinArce <gcamiloholguinarce@gmail.com>
2021-02-16 13:31:26 -0500

    chore: se crea carpetas de proyecto

---
+++ cvrp_project_vue/src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+    <b-container>
+        <b-row>
+            <b-cols>
+                <h1>proyecto CVRP</h1>
+            </b-cols>
+        </b-row>
+        <b-row>
+            <b-col v-for='red in redes' :key="red.red">
+                <b-link :href="red.url" tarjet= '_blanck'>
+                    <b-alert :variant='red.color' show>
+                        {{red.red}}
+                    </b-alert>
+                </b-link>
+
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data(){
+      return{
+          redes:[
+              {
+                  red:'CVRP/FACEBOOK',
+                  url:'https://www.facebook.com',
+                  color:'info'  
+              },
+              {
+                  red:'CVRP/INSTAGRAM',
+                  url:'https://www.INSTAGRAM.com',
+                  color:'warning'
+              },
+              {
+                  red:'CVRP/TWITTER',
+                  url:'https://www.TWITTER.com',
+                  color:'danger'
+              }
+          ]
+      }
+  }
+      }
+</script>
+
+<style 
+
+
+ scoped>
+
+</style>
